e9df5b31c9b9df0dab4a5129d7576ea11a0f2ba4
tyler <tyler@tytonmedia.com>
2018-02-26 16:44:18 -0600

    favicon and buttons
    favicon and buttons

--- resources/views/accounts/settings.blade.php
+++ resources/views/accounts/settings.blade.php
@@ -7,7 +7,7 @@
 				<div class="panel-body">
 					<div class="row">
 						<div class="col-md-6 col-sm-6 col-xs-6">
-							<h2 class="title">Properties</h2>
+							<h1 class="title">Properties</h1>
 						</div>
 						<div class="col-md-6 col-sm-6 col-xs-6 text-right greeting-button">
 							<a href="{{ route('reports.create') }}" class="btn btn-black">Create Report</a>
