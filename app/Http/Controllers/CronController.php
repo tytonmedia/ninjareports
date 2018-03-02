7dfecaf60643f04c1e7c3a5d39d2b036a5199713
tyler <tyler@tytonmedia.com>
2018-03-02 16:41:23 -0600

    refix
    refix

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -555,8 +555,8 @@ class CronController extends Controller
                     if (count($top_5_campaigns_array) > 0) {
                         $top_5_campaigns .= '<table width="100%" cellpadding="5" cellspacing="0" style="background:#fff"><tbody><tr><th style="background:#666;color:#fff;padding:5px;">Campaign</th><th style="background:#666;color:#fff;padding:5px;">Clicks</th><th style="background:#666;color:#fff;padding:5px;">Impressions</th><th style="background:#666;color:#fff;padding:5px;">CTR</th><th style="background:#666;color:#fff;padding:5px;">CPM</th><th style="background:#666;color:#fff;padding:5px;">CPC</th></tr>';
                         foreach ($top_5_campaigns_array as $campaign_array) {
-                            $ad_cpc =  number_format((float) ($campaign_array[6] / 100), 2, '.', '');
-                            $ad_cpm = number_format((float) ($campaign_array[5] / 100), 2, '.', '')
+                            $ad_cpc =  number_format((float) ($campaign_array[6] / 100), 2);
+                            $ad_cpm = number_format((float) ($campaign_array[5] / 100), 2)
                             $top_5_campaigns .= '<tr><td>' . $campaign_array[0] . '</td><td>' . $campaign_array[1] . '</td><td>' . $campaign_array[2] . '</td><td>' . $campaign_array[3] . '</td><td>$' . $ad_cpm  . '</td><td>$' . $ad_cpc . '</td></tr>';
                         }
                         $top_5_campaigns .= '</tbody></table>';
@@ -574,8 +574,8 @@ class CronController extends Controller
                         '%impressions%' => (string) $total_impressions,
                         '%ctr%' => (string) $total_ctr,
                         '%spend%' => (string) number_format((float) ($total_spend / 1000000), 2, '.', ''),
-                        '%page_per_visits%' => (string) number_format((float) ($total_cpm / 100),2, '.',''),
-                        '%new_visitors%' => (string) number_format((float) ($total_cpc / 100),2, '.',''),
+                        '%page_per_visits%' => (string) number_format((float) ($total_cpm / 100),2),
+                        '%new_visitors%' => (string) number_format((float) ($total_cpc / 100),2),
                         '%devices_graph_url%' => (string) $devices_graph_url,
                         '%locations_graph_url%' => (string) $locations_graph_url,
                         '%top_5_campaigns%' => (string) $top_5_campaigns,
