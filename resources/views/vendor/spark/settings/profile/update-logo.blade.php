bef3b439feb9ee0f2c823aab19dd9094f0fd7a11
tyler <tyler@tytonmedia.com>
2018-03-03 11:57:50 -0600

    another fix to white label size
    another fix to white label size

--- resources/views/vendor/spark/settings/profile/update-logo.blade.php
+++ resources/views/vendor/spark/settings/profile/update-logo.blade.php
@@ -11,28 +11,28 @@
                 <form class="form-horizontal" role="form">
                     <!-- Photo Preview-->
                     <div class="form-group">
-                        <label class="col-md-2 control-label">&nbsp;</label>
+                        <label class="col-md-3 control-label">&nbsp;</label>
 
                         <div class="col-md-6">
                             <span role="img" class="profile-photo-preview white-label-logo"
                                 :style="previewStyle">
                             </span>
                         </div>
-                         <label class="col-md-2 control-label">&nbsp;</label>
+                         <label class="col-md-3 control-label">&nbsp;</label>
                     </div>
 
                     <!-- Update Button -->
                     <div class="form-group">
-                        <label class="col-md-2 control-label">&nbsp;</label>
+                        <label class="col-md-3 control-label">&nbsp;</label>
 
-                        <div class="col-md-6">
+                        <div class="col-md-6" style="text-align:center;">
                             <label type="button" class="btn btn-primary btn-upload" :disabled="form.busy">
                                 <span>Select New logo</span>
 
                                 <input ref="logo" type="file" class="form-control" name="logo" @change="update">
                             </label>
                         </div>
-                         <label class="col-md-2 control-label">&nbsp;</label>
+                         <label class="col-md-3 control-label">&nbsp;</label>
                     </div>
                 </form>
             </div>
