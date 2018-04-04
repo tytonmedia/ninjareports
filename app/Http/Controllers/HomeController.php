a5887f2c8a225f3ab1a8402849e4e7352e1177a8
tyler <tyler@tytonmedia.com>
2018-04-04 15:27:57 -0500

    zendesk chat
    zendesk chat

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -33,11 +33,11 @@ class HomeController extends Controller
         $current_plan = auth()->user()->current_billing_plan ? auth()->user()->current_billing_plan : 'free_trial';
         $plan = Plan::whereTitle($current_plan)->first();
         $reports_sent_count = Schedule::whereUserId(auth()->user()->id)->whereBetween('created_at', [date('Y-m-01 00:00:00'), date('Y-m-t 00:00:00')])->count();
-        $reports_sent_count = $reports_sent_count > $plan->reports ? $plan->reports : $reports_sent_count;
+        $reports_sent_count = $reports_sent_count > $plan['reports'] ? $plan['reports'] : $reports_sent_count;
         $active_accounts = Account::where('user_id', auth()->user()->id)->where('status', 1)->count();
 
         $paused = false;
-        if ($reports_sent_count >= $plan->reports) {
+        if ($reports_sent_count >= $plan['reports']) {
             $paused = true;
         }
         return view('home', compact('active_accounts', 'plan', 'reports_sent_count','paused'));
