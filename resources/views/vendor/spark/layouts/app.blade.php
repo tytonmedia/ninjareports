1b1cd3b248ea93e7d8ac330206205507806dcf25
tyler <tyler@tytonmedia.com>
2018-07-30 16:43:48 -0500

    fb tracking
    fb tracking

--- resources/views/vendor/spark/layouts/app.blade.php
+++ resources/views/vendor/spark/layouts/app.blade.php
@@ -74,6 +74,8 @@ src="https://www.facebook.com/tr?id=223802198014729&ev=PageView
 </noscript>
 <!-- End Facebook Pixel Code -->
 
+<script> window.fbAsyncInit = function() { FB.init({ appId : '1985172531761844', xfbml : true, version : 'v3.1' }); FB.AppEvents.logPageView(); }; (function(d, s, id){ var js, fjs = d.getElementsByTagName(s)[0]; if (d.getElementById(id)) {return;} js = d.createElement(s); js.id = id; js.src = "https://connect.facebook.net/en_US/sdk.js"; fjs.parentNode.insertBefore(js, fjs); }(document, 'script', 'facebook-jssdk')); </script>
+
 </head>
 <body class="with-navbar">
     <script>
